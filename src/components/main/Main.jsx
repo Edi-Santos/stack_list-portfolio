fa925fe206184cdacc2fe679981a9be39123bbb2
Ed Santos <edsantos2822@gmail.com>
2022-08-13 16:32:36 -0300

    style:(Main.jsx): diminuindo linhas da função stacks()

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,9 +2,7 @@ import Card from './Card';
 import datas from '../../datas';
 
 const stacks = () => datas.map(
-  (data, index) => {
-    return <Card key={index} name={data.name} imgPath={data.imgPath} />
-  }
+  (data, index) => <Card key={index} name={data.name} imgPath={data.imgPath} />
 );
 
 function Main() {
