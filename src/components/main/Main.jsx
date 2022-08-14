6db235d58066dec93ea19f577d74bd63776dd1fe
Ed Santos <edsantos2822@gmail.com>
2022-08-14 19:05:54 -0300

    chore:(Main.jsx): importando o arquivo main.css

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,6 +1,8 @@
 import Card from './Card';
 import datas from '../../datas';
 
+import './main.css';
+
 const stacks = () => datas.map(
   (data, index) => <Card key={index} name={data.name} imgPath={data.imgPath} />
 );
