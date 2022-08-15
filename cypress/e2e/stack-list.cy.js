60f4fdc6d92b4a54df66173a1af8dc3762efede3
Ed Santos <edsantos2822@gmail.com>
2022-08-15 14:33:09 -0300

    test:(stack-list.cy.js): implementando teste 1

--- cypress/e2e/stack-list.cy.js
+++ cypress/e2e/stack-list.cy.js
@@ -1,5 +1,15 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="cypress" />
+
+const HEADER = 'header';
+const HEADER_H1 = `${HEADER} h1`;
+
+describe('Testando o Projeto Stack List', () => {
+  before(() => {
+    cy.visit('http://localhost:3000/')
+  });
+
+  it('1 - verifica se há um <header /> e se há um título <h1 />', () => {
+    cy.get(HEADER).should('exist');
+    cy.get(HEADER_H1).should('exist');
+  });
+});
