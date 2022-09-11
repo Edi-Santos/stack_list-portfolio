d514722c5d32db46cfa734708e5418061a64fe05
Ed Santos <edsantos2822@gmail.com>
2022-09-10 22:10:12 -0300

    build: separando o projeto pronto na pasta base_answer

---
+++ base_answer/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+
+import './app.css';
+import './var.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
